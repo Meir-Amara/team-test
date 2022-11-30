2fec33934648ff4f1ad7c257dc3b6c1395d09d6a
Meir Amara <meir757@gmail.com>
2022-11-30 21:16:51 +0200

    str=>obj

--- server/routers/meirRouter.js
+++ server/routers/meirRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/meir",(req,res)=>res.json({name:"meir"}))
+router.get("/meir",(req,res)=>res.json({name: "meir"}))
 
 module.exports=router
