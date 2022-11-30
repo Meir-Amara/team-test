da45f69881556989a4f53109cec7df5efdc28185
Wubet zemele <wubet777@gmail.com>
2022-11-30 23:26:11 +0200

    massge

--- server/app.js
+++ server/app.js
@@ -5,4 +5,8 @@ const cors=require('cors')
 const routerMeir=require('./routers/meirRouter')
 app.use('/test',routerMeir)
 
+//  wubet route
+const wubetRoute = require('./routers/wubetRouter')
+app.use('/bamba',wubetRoute)
+
 app.listen(8000,console.log("connect to port 8000"))
