8c8ba870c95edb1efad20b23b2c75d16c38294f1
chen wassi <chenwassi1@gmail.com>
2022-12-01 01:20:33 +0200

    chen branch

--- server/routers/meirRouter.js
+++ server/routers/meirRouter.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/meir",(req,res)=>res.json({name: "meir amara"}))
+router.get("/chen",(req,res)=>res.json({name: "chen wassi"}))
 
 module.exports=router
