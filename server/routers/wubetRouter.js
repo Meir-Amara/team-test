461785f75fb29e506a8c63c6709d5d9cbc0123ab
Wubet zemele <wubet777@gmail.com>
2022-11-30 23:49:10 +0200

    lname

--- server/routers/wubetRouter.js
+++ server/routers/wubetRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
 const router =express.Router()
-router.get('/wubet',(req,res)=>res.json({name:"wubet"}))
+router.get('/wubet',(req,res)=>res.json({name:"wubet zemele"}))
 
 module.exports = router
