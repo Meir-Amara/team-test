ec26c3fc22b5be8bd293081cb2217a6212bf72bd
Meir Amara <meir757@gmail.com>
2022-12-01 12:38:57 +0200

    c

--- server/routers/wubetRouter.js
+++ server/routers/wubetRouter.js
@@ -3,4 +3,4 @@ const express = require('express');
 const router =express.Router()
 router.get('/wubet',(req,res)=>res.json({name:"wubet zemele"}))
 
-module.exports = router
+module.exports = router 
